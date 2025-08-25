532ba08d6d10354b660c0c9c333d0f1273b38eea
Wolfgang Maier <maierw@informatik.uni-freiburg.de>
2025-08-25 13:28:16 +0200

    Fix parameter help

--- vapor.py
+++ vapor.py
@@ -155,7 +155,7 @@ def cli():
     parser.add_argument("--return_best_n", type=int, default=1)
     parser.add_argument("-m", "--min_kmer_prop", type=float, help="Minimum proportion of matched kmers allowed for queries [default=0.1]", nargs='?', default=0.1)
     parser.add_argument("-k", type=int, help="Kmer Length [5 > int > 30, default=21]", nargs='?', default=21)
-    parser.add_argument("-t", "--threshold", type=float, help="Read kmer filtering threshold [0 > float > 1, default=0.0]", nargs='?', default=0.2)
+    parser.add_argument("-t", "--threshold", type=float, help="Read kmer filtering threshold [0 > float > 1, default=0.2]", nargs='?', default=0.2)
     parser.add_argument("-c", "--min_kmer_cov", type=float, help="Minimum coverage kmer culling [default=5]", nargs='?', default=5)
     parser.add_argument("-fa", type=str, help="Fasta file")
     parser.add_argument("-fq", nargs='+', type=str, help="Fastq file/files")
